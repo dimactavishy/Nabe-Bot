6470163db1a484731df7b4376c679426f9593c03
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-11 09:52:07 +0700

    Create gbooru.js

---
+++ commands/imageboard/gbooru.js
@@ -0,0 +1,42 @@
+const Booru = require('booru')
+const Color = `RANDOM`;
+module.exports = {
+    info: {
+        name: "gbooru",
+        description: "gbooru image scraper",
+        cooldown: 30,
+    },
+    async execute(client, message, args, Discord) {
+        const image_query = args.join(' ');
+        if (!message.content.includes('help'))
+            Booru.search('gelbooru.com', image_query, { limit: 1, random: true })
+                .then(posts => {
+                    if (posts.length === 0) {
+                        const notfoundEmbed = new Discord.MessageEmbed()
+                            .setDescription("Sorry, i found no results for what you're looking for.")
+                            .setFooter("Are you sure you didn't do a typo?")
+                        message.channel.send(notfoundEmbed)
+                    }
+
+                    for (let post of posts) {
+                        const booruEmbed = new Discord.MessageEmbed()
+                            .setColor('GREEN')
+                            .setTitle("Here's an image for you, master.")
+                            .setDescription("I hope this result satisfies you.")
+                            .addField('Provided by Gelbooru.com', `[Booru Page](${post.postView})`)
+                            .setThumbnail('https://media.discordapp.net/attachments/898563395807232061/899534056356724756/sketch-1634535999710.png?width=499&height=499')
+                            .setImage(post.fileUrl)
+                            .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
+                            .setTimestamp();
+
+                        message.channel.send(booruEmbed);
+                    }
+                })
+        if (message.content.includes('help')) {
+            const helpEmbed = new Discord.MessageEmbed()
+                .addField('Returns an image from gelbooru', '`nabe gbooru <optional query>`')
+                .setFooter('Example: nabe gbooru narberal_gamma')
+            message.channel.send(helpEmbed)
+        }
+    }
+}
