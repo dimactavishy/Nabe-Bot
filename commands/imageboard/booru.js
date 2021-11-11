a8727e4465ac3a0e9de87584cadb44e546c6b653
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-11 11:06:51 +0700

    Update booru.js

--- commands/imageboard/booru.js
+++ commands/imageboard/booru.js
@@ -11,8 +11,9 @@ module.exports = {
         if (!message.content.includes('help')){
             const hornyEmbed = new Discord.MessageEmbed()
                 .setTitle('Potential Lewd Warning')
-                .setDescription('The `nabe booru` command has a potential to return a NSFW image, please use this command in a NSFW channel just to be safe.\n'
-                               + 'If you really want to use booru on a SFW channel, use `nabe boorusafe` instead.'
+                .setDescription('**The `nabe booru` command has a potential to return a NSFW image.\n**'
+                               + 'Please use this command in a NSFW channel just to be safe.\n'
+                               + '_If you really want to use booru on a SFW channel, use `nabe boorusafe` instead._'
                                )
                 .setImage('https://static.wikia.nocookie.net/isekai-quartet/images/9/9c/Narberal_Gamma.png/revision/latest?cb=20200313155041')
                 .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
