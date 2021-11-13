893b32de6d1b18329e8e1e676019db752768d3df
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-13 16:28:16 +0700

    Update paheal.js

--- commands/nsfw/paheal.js
+++ commands/nsfw/paheal.js
@@ -30,9 +30,9 @@ module.exports = {
                             .setTitle('P-Pervert!')
                             .setColor('#FFC0CB')
                             .setDescription("H-here's the image i found on paheal...")
-                            .addField('Provided by Rule34.paheal.net', `[Booru Page](${postRandom.postView})`)
+                            .addField('Provided by Rule34.paheal.net', `[Booru Page](${post.postView})`)
                             .setThumbnail('https://media.discordapp.net/attachments/898563395807232061/899534056356724756/sketch-1634535999710.png?width=499&height=499')
-                            .setImage(postRandom.fileUrl)
+                            .setImage(post.fileUrl)
                             .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                             .setTimestamp();
 
