815829fe4b2c17ac5d0bc9884e9ad6a5cdcb84f9
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-13 16:25:21 +0700

    Update paheal.js

--- commands/nsfw/paheal.js
+++ commands/nsfw/paheal.js
@@ -16,7 +16,7 @@ module.exports = {
                     .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                     .setTimestamp();
                 if (!message.channel.nsfw) return message.channel.send(hornyEmbed)
-                Booru.search('gelbooru.com', image_query, { limit: 1, random: true })
+                Booru.search('gelbooru.com', image_query, { limit: 1, random: false })
                 .then(posts => {
                     if (posts.length === 0) {
                         const notfoundEmbed = new Discord.MessageEmbed()
