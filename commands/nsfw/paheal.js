a5888b29f2ad5ff659caa5f0b16c02b6817aa281
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-15 11:42:40 +0700

    Update paheal.js

--- commands/nsfw/paheal.js
+++ commands/nsfw/paheal.js
@@ -9,8 +9,8 @@ module.exports = {
     },
     async execute(client, message, args, Discord) {
         const tag_query = args.join(' ');
-        if (!message.content.includes('narberal_gamma')) {
         if (!message.content.includes('help')) {
+            if (!message.content.includes('narberal_gamma')) {
             const hornyEmbed = new Discord.MessageEmbed()
             .setTitle('No lewding here!')
             .setDescription('Not everybody is a pervert like you, please do it in a NSFW channel.')
@@ -41,20 +41,20 @@ module.exports = {
                }
             })
         }
-        }
-        if (message.content.includes('help')) {
-            const helpEmbed = new Discord.MessageEmbed()
-                .addField('Returns an image from rule34.paheal', '`nabe paheal <optional query>`')
-                .setFooter('Example: nabe paheal narberal_gamma')
-            message.channel.send(helpEmbed)
-        }
-        if (message.content.includes('narberal_gamma')) {
+            if (message.content.includes('narberal_gamma')) {
             const helpEmbed = new Discord.MessageEmbed()
                 .setTitle('No Lewding Nabe!')
                 .setDescription('I will burn you like a roasted chicken if you still insist on lewding me.')
                 .setImage('https://user-images.githubusercontent.com/79780581/140713738-91db6652-53ab-4be7-b57e-7eb8a1bd5c8e.jpg')
                 .setFooter('Very Angry Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                 .setTimestamp();
+            message.channel.send(helpEmbed)
+        }
+        }
+        if (message.content.includes('help')) {
+            const helpEmbed = new Discord.MessageEmbed()
+                .addField('Returns an image from rule34.paheal', '`nabe paheal <optional query>`')
+                .setFooter('Example: nabe paheal narberal_gamma')
             message.channel.send(helpEmbed)
         }
     }
