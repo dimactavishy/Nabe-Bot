405794c08b6eee855817946dce2cf2963559d59a
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-15 11:41:09 +0700

    Update paheal.js

--- commands/nsfw/paheal.js
+++ commands/nsfw/paheal.js
@@ -9,6 +9,7 @@ module.exports = {
     },
     async execute(client, message, args, Discord) {
         const tag_query = args.join(' ');
+        if (!message.content.includes('narberal_gamma')) {
         if (!message.content.includes('help')) {
             const hornyEmbed = new Discord.MessageEmbed()
             .setTitle('No lewding here!')
@@ -31,7 +32,7 @@ module.exports = {
                   .setTitle('P-Pervert!')
                   .setColor('#FFC0CB')
                   .setDescription(`H-Here's something i found on paheal!`)
-                  .addField(`Provided by Rule34.paheal.net`, `[Full-Res Image](${post.postView})`)
+                  .addField(`Provided by Rule34.paheal.net`, `[Booru Page](${post.postView})`)
                   .setThumbnail('https://media.discordapp.net/attachments/898563395807232061/907183711882199040/sketch-1636359767759.png?width=499&height=499')
                   .setImage(post.fileUrl)
                   .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
@@ -40,10 +41,20 @@ module.exports = {
                }
             })
         }
+        }
         if (message.content.includes('help')) {
             const helpEmbed = new Discord.MessageEmbed()
                 .addField('Returns an image from rule34.paheal', '`nabe paheal <optional query>`')
                 .setFooter('Example: nabe paheal narberal_gamma')
+            message.channel.send(helpEmbed)
+        }
+        if (message.content.includes('narberal_gamma')) {
+            const helpEmbed = new Discord.MessageEmbed()
+                .setTitle('No Lewding Nabe!')
+                .setDescription('I will burn you like a roasted chicken if you still insist on lewding me.')
+                .setImage('https://user-images.githubusercontent.com/79780581/140713738-91db6652-53ab-4be7-b57e-7eb8a1bd5c8e.jpg')
+                .setFooter('Very Angry Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
+                .setTimestamp();
             message.channel.send(helpEmbed)
         }
     }
