40cfe73c2c3327a6aa91ffd4c2ae669ae66c3c9d
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-15 10:43:55 +0700

    Update r34.js

--- commands/nsfw/r34.js
+++ commands/nsfw/r34.js
@@ -21,7 +21,7 @@ module.exports = {
                 .setTimestamp();
             if (!message.channel.nsfw) return message.channel.send(hornyEmbed);
 
-           Booru.search('rule34.xxx', tag_query, { limit: 10, random: false })
+           Booru.search('rule34.paheal.net', tag_query, { limit: 2, random: false })
            .then(posts => {
              if (posts.length === 0) {
              console.log('No images found.')
