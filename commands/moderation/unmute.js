294f031d4e00118d544d04c20a17a1c9ce8c1c7f
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-15 11:59:35 +0700

    Update unmute.js

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -5,6 +5,7 @@ module.exports = {
     
         const target = message.mentions.users.first();
         if (!message.content.includes('help'))
+        if (message.member.hasPermission('MUTE_MEMBERS')) {
         if (target) {
             let mainRole = message.guild.roles.cache.find(role => role.name === 'Verified');
             let muteRole = message.guild.roles.cache.find(role => role.name === 'Muted');
@@ -16,6 +17,9 @@ module.exports = {
             message.channel.send(`<@${memberTarget.user.id}>, you are now permitted to speak.`);
         } else{
             message.reply("please mention the person that you want to forgive and be allowed to speak again.").then(msg => { msg.delete({ timeout: 5000 }) });
+        }
+        }else {
+            message.reply("i'm sorry, but you do not have the authority to kick members.").then(msg => { msg.delete({ timeout: 5000 }) });
         }
         if (message.content.includes('help')) {
             const helpEmbed = new Discord.MessageEmbed()
