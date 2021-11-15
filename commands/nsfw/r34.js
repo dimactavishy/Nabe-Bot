8b6fba3082cfc74b0b0e307e806216d5d759423e
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-15 11:30:45 +0700

    Update r34.js

--- commands/nsfw/r34.js
+++ commands/nsfw/r34.js
@@ -31,9 +31,9 @@ module.exports = {
                   .setTitle('P-Pervert!')
                   .setColor('#FFC0CB')
                   .setDescription(`H-Here's something i found on rule34!`)
-                  .addField(`Provided by Rule34.xxx`, `[Full-Res Image](${post.postView})`)
+                  .addField(`Provided by Rule34.xxx`, `[Booru Page](${post.postView})`)
                   .setThumbnail('https://media.discordapp.net/attachments/898563395807232061/907183711882199040/sketch-1636359767759.png?width=499&height=499')
-                  .setImage(post.fileURL)
+                  .setImage(post.fileUrl)
                   .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                   .setTimestamp();
                 message.channel.send(embed_nsfw);
