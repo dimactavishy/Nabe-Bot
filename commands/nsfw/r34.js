bdd2c0639e4f82be56c17663ecafb51267c8881f
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-18 08:55:40 +0700

    Update r34.js

--- commands/nsfw/r34.js
+++ commands/nsfw/r34.js
@@ -62,10 +62,10 @@ module.exports = {
                             embed_nsfw = new Discord.MessageEmbed()
                                 .setTitle('P-Pervert!')
                                 .setColor('#FFC0CB')
-                                .setAuthor('Provided by Rule34.xxx', 'https://rule34.xxx/favicon.ico', 'https://rule34.xxx')
+                                .setAuthor('Provided by Rule34.xxx', `https://www.${siteUrl}/favicon.ico`, 'https://rule34.xxx')
                                 .setDescription(`H-Here's something i found on rule34!\n`
                                     + `**Provided by:** Rule34.xxx | `,
-                                    +`[**Booru Page**](${post.postView}) | `,
+                                    + `[**Booru Page**](${post.postView}) | `,
                                     + `**Rating:** ${posts.rating}.toUpperCase() | `,
                                     + `**File:** ${path.extname(post.fileUrl).toLowerCase()}, ${headers ? fileSizeSI(headers.get('content-length')) : '? kB'}\n`,
                                     + `**Tags:** ${tags}`,
