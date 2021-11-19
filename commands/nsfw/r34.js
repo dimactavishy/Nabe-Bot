9de1a2ea8469e401e4114afa72128c6a5bba4ef7
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-19 14:58:33 +0700

    Update r34.js

--- commands/nsfw/r34.js
+++ commands/nsfw/r34.js
@@ -74,7 +74,7 @@ module.exports = {
                                 .setColor('#FFC0CB')
                                 .setDescription(`H-Here's something i found on rule34!\n` +
                                     `**Rule34.xxx** | ` +
-                                    `[𝗕𝗼𝗼𝗿𝘂 𝗣𝗮𝗴𝗲](${post.postView}) | ` +
+                                    `[**Booru Page**](${post.postView}) | ` +
                                     `**Rating:** ${post.rating.toUpperCase()} | ` +
                                     `**File:** ${path.extname(post.fileUrl).toUpperCase()}, ${headers ? fileSizeSI(headers.get('content-length')) : '? kB'}\n` +
                                     `**Tags:** ${tags}\n\n` +
