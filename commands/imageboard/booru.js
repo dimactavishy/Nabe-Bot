a85aa7ac970d19b9193be17b24677c186ef9ca84
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-19 15:00:54 +0700

    Update booru.js

--- commands/imageboard/booru.js
+++ commands/imageboard/booru.js
@@ -1,5 +1,8 @@
+const fetch = require('node-fetch')
 const Booru = require('booru')
 const Discord = require('discord.js')
+const { escapeMarkdown } = Discord.Util
+const path = require('path')
 const Color = `RANDOM`;
 module.exports = {
     info: {
@@ -8,7 +11,19 @@ module.exports = {
         cooldown: 30,
     },
     async execute(client, message, args, Discord) {
-        const image_query = args.join(' ');
+        const fetch = require('node-fetch')
+        const tag_query = args.join(' ');
+        
+        function fileSizeSI(a, b, c, d, e) {
+            return (
+                ((b = Math),
+                    (c = b.log),
+                    (d = 1e3),
+                    (e = (c(a) / c(d)) | 0),
+                    a / b.pow(d, e)).toFixed(0) + (e ? 'kMGTPEZY'[--e] + 'B' : ' Bytes')
+            )
+        }
+        
         if (!message.content.includes('help')){
             const hornyEmbed = new Discord.MessageEmbed()
                 .setTitle('Potential Lewd Warning')
@@ -20,27 +35,83 @@ module.exports = {
                 .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                 .setTimestamp();
             if (!message.channel.nsfw) return message.channel.send(hornyEmbed);
-            Booru.search('gelbooru.com', image_query, { limit: 1, random: true })
-                .then(posts => {
-                    if (posts.length === 0) {
-                        const notfoundEmbed = new Discord.MessageEmbed()
-                            .setDescription("Sorry, i found no results for what you're looking for.")
-                            .setFooter("Are you sure you didn't do a typo?")
-                        message.channel.send(notfoundEmbed)
-                    }
+            
+            Booru.search('gelbooru.com', tag_query, { limit: 1, random: true })
+                .then(async posts => {
+                        if (posts.length === 0) {
+                            const notfoundEmbed = new Discord.MessageEmbed()
+                                .setDescription("Sorry, i found no results for what you're looking for.")
+                                .setFooter("Are you sure you didn't do a typo?")
+                            message.channel.send(notfoundEmbed)
+                        }
 
                     for (let post of posts) {
+                        
+                        let tags =
+                                post.tags.join(', ').length < 50
+                                    ? Discord.Util.escapeMarkdown(post.tags.join(', '))
+                                    : Discord.Util.escapeMarkdown(post.tags.join(', ').substr(0, 50)) +
+                                    `... [See All](https://giraffeduck.com/api/echo/?w=${Discord.Util
+                                        .escapeMarkdown(post.tags.join(',').replace(/(%20)/g, '_'))
+                                        .replace(/([()])/g, '\\$1')
+                                        .substring(0, 1200)})`
+
+                            let headers
+                            let tooBig = false
+                            let imgError = false
+
+                            try {
+                                headers = (await fetch(post.fileUrl, { method: 'HEAD' })).headers
+                            } catch (e) {
+                                imgError = true
+                            }
+
+                            if (headers) {
+                                tooBig = parseInt(headers.get('content-length'), 50) / 1000000 > 10
+                            }
+                        
                         const booruEmbed = new Discord.MessageEmbed()
                             .setColor('YELLOW')
                             .setTitle("Here's an image for you, master.")
-                            .setDescription("I hope this result satisfies you.")
-                            .addField('Provided by Gelbooru.com', `[Booru Page](${post.postView})`)
+                            .setDescription(`I hope this result satisfies you.\n` +
+                                    `**Gelbooru.com** | ` +
+                                    `[**Booru Page**](${post.postView}) | ` +
+                                    `**Rating:** ${post.rating.toUpperCase()} | ` +
+                                    `**File:** ${path.extname(post.fileUrl).toUpperCase()}, ${headers ? fileSizeSI(headers.get('content-length')) : '? kB'}\n` +
+                                    `**Tags:** ${tags}\n\n` +
+                                    (!['.jpg', '.jpeg', '.png', '.gif'].includes(
+                                        path.extname(post.fileUrl).toLowerCase(),
+                                    )
+                                        ? '**`The file is perhaps a video and will not be embeddable.`**\n'
+                                        : '') +
+                                    (tooBig && ['.jpg', '.jpeg', '.png', '.gif'].includes(
+                                        path.extname(post.fileUrl).toLowerCase(),
+                                    )
+                                        ? '**`The image is over 50MB and will not be embeddable.`**\n' : '') +
+                                    (imgError ? '**`Sorry, but there was an error getting the file.\n`**' : ''),
+                                           
+                                           )
                             .setThumbnail('https://media.discordapp.net/attachments/898563395807232061/899534056356724756/sketch-1634535999710.png?width=499&height=499')
                             .setImage(post.fileUrl)
                             .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
                             .setTimestamp();
 
                         message.channel.send(booruEmbed);
+                        
+                        if (!['.jpg', '.jpeg', '.png', '.gif'].includes(
+                                path.extname(post.fileUrl).toLowerCase())) {
+                                message.channel.send(`*The file is not embeddable, so here's the link instead:*\n`
+                                + post.fileUrl
+                               )
+                            }
+
+                            if (tooBig && ['.jpg', '.jpeg', '.png', '.gif'].includes(
+                                path.extname(post.fileUrl).toLowerCase())) {
+                                message.channel.send(`*The file is over 10MB, so here's the link instead:*\n`
+                                + post.fileUrl
+                               )
+                            }
+                        
                     }
                 })
         }
