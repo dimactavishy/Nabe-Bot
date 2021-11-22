6ef3d2f7d5aa475e8a844bd51640dc11f064ef98
Joko Sukino <dimasadyuta6@gmail.com>
2021-11-22 20:07:17 +0700

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -11,12 +11,17 @@ module.exports = {
 
   execute: async function (client, message, args) {
     const channel = message.member.voice.channel
-    if (!channel)return message.reply("i'm sorry but you need to be in a voice channel to play music!").then(msg => { msg.delete({ timeout: 5000 }) });
+    if (!channel) return message.reply("i'm sorry but you need to be in a voice channel to play music!").then(msg => { msg.delete({ timeout: 5000 }) });
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return message.reply("you have not ordered me to play anything yet.").then(msg => { msg.delete({ timeout: 5000 }) });
+    if (!serverQueue) return message.reply("you have not ordered me to play anything yet.").then(msg => { msg.delete({ timeout: 5000 }) });
     serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end("I'll gladly play music for you again, master!");
-    message.react("✅")
+    try {
+      serverQueue.connection.dispatcher.end("I'll gladly play music for you again, master!");
+      message.react("✅")
+    } catch (err) {
+      message.channel.send("I'm sorry, i think i made a mistake there...");
+      console.log(err);
+    }
 
   }
 };
