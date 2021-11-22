18f1ddd9c8a83e329ec9d94dbc39bb3c04d53587
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-22 14:21:12 +0700

    Update nabe.js

--- nabe.js
+++ nabe.js
@@ -8,7 +8,7 @@ client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
 client.queue = new Map()
 
-fs.readdir(__dirname + "/events/", (err, files) => {
+fs.readdir(__dirname + "./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
     const event = require(__dirname + `/events/${file}`);
@@ -17,7 +17,15 @@ fs.readdir(__dirname + "/events/", (err, files) => {
     console.log("Loading Event: " + eventName)
   });
 });
-
+fs.readdir(__dirname + "./events/guild", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    const guild = require(__dirname + `/events/guild/${file}`);
+    let guildName = file.split(".")[0];
+    client.on(guildName, event.bind(null, client));
+    console.log("Loading Event: " + guildName)
+  });
+});
 fs.readdir("./commands/funstuff", (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
