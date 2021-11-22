1238773bd2d609d934d25a1f665d475b438c8c72
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2021-11-22 15:33:47 +0700

    Update anime.js

--- commands/search/anime.js
+++ commands/search/anime.js
@@ -6,6 +6,7 @@ const Scraper = require("mal-scraper"); //npm i mal-scraper
 module.exports = {
   info: {
     name: "anime",
+    cooldown: "30",
     description: "",
     usage: "",
   },
@@ -50,11 +51,11 @@ module.exports = {
           .setThumbnail(Anime.picture)
           .setFooter('Egg-Shaped Battle Maid', 'https://images-ext-2.discordapp.net/external/l7-PY5Kkvta4_p-sOE0ftwQCmJ9iAe72eMPSTczuWi0/%3Fsize%3D512/https/cdn.discordapp.com/avatars/897674562265817088/e36ef03370367a4b3cd51b864e9df392.png?width=499&height=499')
           .setTimestamp();
-
+message.channel.send(Embed)
       } catch (error) {
         return message.channel.send(`Unfortunately, i couldn't find what you're looking for.`).then(msg => { msg.delete({ timeout: 5000 }) });
       };
-      message.channel.send(Embed);
+      
       await Msg.delete();
     }
 
