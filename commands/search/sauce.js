1bb65a2798a914785376a7e72b787874c69f56db
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2022-01-14 03:52:58 -0500

    Update sauce.js

--- commands/search/sauce.js
+++ commands/search/sauce.js
@@ -12,18 +12,17 @@ module.exports = {
 
         let mySauce = sagiri("05d0753bbec2836f3709bef097fda361857a8a62");
 
-        if (message.attachments) {
-            let file = message.attachments.first();
+        if (message.attachments.size > 0) {
 
             message.attachments.forEach(attachment => {
-                var imageurl = attachment.url;
-              });
-
-            mySauce(imageurl, { results: 1 })
+                let imageurl = attachment.url;
+                mySauce(imageurl, { results: 10 })
                 .then(response => {
                     console.log(response);
                     if (response.length < 1) return message.channel.send("No image was found!");
                 });
+              });
+
         }
         //const results = await client("http://i.imgur.com/5yFTeRV.png");
 
