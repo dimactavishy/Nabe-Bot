b2410c7be7ee388488b67860e76aa164a6d44cce
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2022-01-17 07:04:11 -0500

    Update sauce.js

--- commands/search/sauce.js
+++ commands/search/sauce.js
@@ -18,14 +18,14 @@ module.exports = {
             message.attachments.forEach(attachment => {
                 let imageurl = attachment.url;
 
-                let amount = args.join( );
+                let amount = args.join();
 
                 if (!amount) amount = 10;
                 if (amount > 7) return message.reply("I'm sorry, but the maximum number of sauce results is 7 to avoid channel flooding.").then(msg => { msg.delete({ timeout: 5000 }) });
                 if (amount < 0) return message.reply("Forgive me, but specifying a number smaller than one would induce a paradox. So please, specify a greater number.").then(msg => { msg.delete({ timeout: 5000 }) });
                 if (isNaN(amount)) return message.reply("Please only enter a number between 1 to 7 or none at all.").then(msg => { msg.delete({ timeout: 5000 }) });
 
-                mySauce(imageurl, { results: 2 })
+                mySauce(imageurl, { results: 5 })
                     .then(async response => {
 
                         console.log(response);
@@ -36,131 +36,116 @@ module.exports = {
                             const titleEmbed = new Discord.MessageEmbed()
                                 .setColor('#ADD8E6')
                                 .setTitle('Looking For Sauce?')
-                            message.channel.send(titleEmbed)
 
                             //embed 1
-                            if (amount = [1, 2, 3, 4, 5, 6, 7]) {
-                                const sauceEmbed = new Discord.MessageEmbed()
-                                    .setColor('#ADD8E6')
-                                    .setThumbnail(response[0].thumbnail)
-                                    .setDescription(
-                                        `**Result 1**\n`
-                                        + `**By [${response[0].authorName}](${response[0].authorUrl})**\n`
-                                        + `**Site:** ${response[0].site} | `
-                                        + `**Similarity:** ${response[0].similarity}\n`
-                                        + `**[${response[0].site} Page](${response[0].url})**`
-                                    )
-                                if (response[0].authorName == null) {
-                                    sauceEmbed.setDescription(
-                                        `**Result 1**\n`
-                                        + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                        + `**Site:** ${response[0].site} | `
-                                        + `**Similarity:** ${response[0].similarity}\n`
-                                        + `**[${response[0].site} Page](${response[0].url})**`
-                                    )
-                                }
-                                await message.channel.send(sauceEmbed)
+                            const sauceEmbed = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[0].thumbnail)
+                                .setDescription(
+                                    `**Result 1**\n`
+                                    + `**By [${response[0].authorName}](${response[0].authorUrl})**\n`
+                                    + `**Site:** ${response[0].site} | `
+                                    + `**Similarity:** ${response[0].similarity}\n`
+                                    + `**[${response[0].site} Page](${response[0].url})**`
+                                )
+                            if (response[0].authorName == null) {
+                                sauceEmbed.setDescription(
+                                    `**Result 1**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[0].site} | `
+                                    + `**Similarity:** ${response[0].similarity}\n`
+                                    + `**[${response[0].site} Page](${response[0].url})**`
+                                )
                             }
 
                             //embed 2
-                            if (amount = [2, 3, 4, 5, 6, 7]) {
-                                const sauceEmbed2 = new Discord.MessageEmbed()
-                                    .setColor('#ADD8E6')
-                                    .setThumbnail(response[1].thumbnail)
-                                    .setDescription(
-                                        `**Result 2**\n`
-                                        + `**By [${response[1].authorName}](${response[1].authorUrl})**\n`
-                                        + `**Site:** ${response[1].site} | `
-                                        + `**Similarity:** ${response[1].similarity}\n`
-                                        + `**[${response[1].site} Page](${response[1].url})**`
-                                    )
-                                if (response[1].authorName == null) {
-                                    sauceEmbed2.setDescription(
-                                        `**Result 2**\n`
-                                        + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                        + `**Site:** ${response[1].site} | `
-                                        + `**Similarity:** ${response[1].similarity}\n`
-                                        + `**[${response[1].site} Page](${response[1].url})**`
-                                    )
-                                }
-                                await message.channel.send(sauceEmbed2)
+                            const sauceEmbed2 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[1].thumbnail)
+                                .setDescription(
+                                    `**Result 2**\n`
+                                    + `**By [${response[1].authorName}](${response[1].authorUrl})**\n`
+                                    + `**Site:** ${response[1].site} | `
+                                    + `**Similarity:** ${response[1].similarity}\n`
+                                    + `**[${response[1].site} Page](${response[1].url})**`
+                                )
+                            if (response[1].authorName == null) {
+                                sauceEmbed2.setDescription(
+                                    `**Result 2**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[1].site} | `
+                                    + `**Similarity:** ${response[1].similarity}\n`
+                                    + `**[${response[1].site} Page](${response[1].url})**`
+                                )
                             }
 
                             //embed 3
-                            if (amount = [3, 4, 5, 6, 7]) {
-                                const sauceEmbed3 = new Discord.MessageEmbed()
-                                    .setColor('#ADD8E6')
-                                    .setThumbnail(response[2].thumbnail)
-                                    .setDescription(
-                                        `**Result 3**\n`
-                                        + `**By [${response[2].authorName}](${response[2].authorUrl})**\n`
-                                        + `**Site:** ${response[2].site} | `
-                                        + `**Similarity:** ${response[2].similarity}\n`
-                                        + `**[${response[2].site} Page](${response[2].url})**`
-                                    )
-                                if (response[2].authorName == null) {
-                                    sauceEmbed3.setDescription(
-                                        `**Result 3**\n`
-                                        + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                        + `**Site:** ${response[2].site} | `
-                                        + `**Similarity:** ${response[2].similarity}\n`
-                                        + `**[${response[2].site} Page](${response[2].url})**`
-                                    )
-                                }
-                                await message.channel.send(sauceEmbed3)
+                            const sauceEmbed3 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[2].thumbnail)
+                                .setDescription(
+                                    `**Result 3**\n`
+                                    + `**By [${response[2].authorName}](${response[2].authorUrl})**\n`
+                                    + `**Site:** ${response[2].site} | `
+                                    + `**Similarity:** ${response[2].similarity}\n`
+                                    + `**[${response[2].site} Page](${response[2].url})**`
+                                )
+                            if (response[2].authorName == null) {
+                                sauceEmbed3.setDescription(
+                                    `**Result 3**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[2].site} | `
+                                    + `**Similarity:** ${response[2].similarity}\n`
+                                    + `**[${response[2].site} Page](${response[2].url})**`
+                                )
                             }
 
                             //embed 4
-                            if (amount = [4, 5, 6, 7]) {
-                                const sauceEmbed4 = new Discord.MessageEmbed()
-                                    .setColor('#ADD8E6')
-                                    .setThumbnail(response[3].thumbnail)
-                                    .setDescription(
-                                        `**Result 4**\n`
-                                        + `**By [${response[3].authorName}](${response[3].authorUrl})**\n`
-                                        + `**Site:** ${response[3].site} | `
-                                        + `**Similarity:** ${response[3].similarity}\n`
-                                        + `**[${response[3].site} Page](${response[3].url})**`
-                                    )
-                                if (response[3].authorName == null) {
-                                    sauceEmbed4.setDescription(
-                                        `**Result 4**\n`
-                                        + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                        + `**Site:** ${response[3].site} | `
-                                        + `**Similarity:** ${response[3].similarity}\n`
-                                        + `**[${response[3].site} Page](${response[3].url})**`
-                                    )
-                                }
-                                await message.channel.send(sauceEmbed4)
+                            const sauceEmbed4 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[3].thumbnail)
+                                .setDescription(
+                                    `**Result 4**\n`
+                                    + `**By [${response[3].authorName}](${response[3].authorUrl})**\n`
+                                    + `**Site:** ${response[3].site} | `
+                                    + `**Similarity:** ${response[3].similarity}\n`
+                                    + `**[${response[3].site} Page](${response[3].url})**`
+                                )
+                            if (response[3].authorName == null) {
+                                sauceEmbed4.setDescription(
+                                    `**Result 4**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[3].site} | `
+                                    + `**Similarity:** ${response[3].similarity}\n`
+                                    + `**[${response[3].site} Page](${response[3].url})**`
+                                )
                             }
 
                             //embed 5
-                            if (amount = [5, 6, 7]) {
-                                const sauceEmbed5 = new Discord.MessageEmbed()
-                                    .setColor('#ADD8E6')
-                                    .setThumbnail(response[4].thumbnail)
-                                    .setDescription(
-                                        `**Result 5**\n`
-                                        + `**By [${response[4].authorName}](${response[4].authorUrl})**\n`
-                                        + `**Site:** ${response[4].site} | `
-                                        + `**Similarity:** ${response[4].similarity}\n`
-                                        + `**[${response[4].site} Page](${response[4].url})**`
-                                    )
-                                if (response[4].authorName == null) {
-                                    sauceEmbed5.setDescription(
-                                        `**Result 5**\n`
-                                        + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                        + `**Site:** ${response[4].site} | `
-                                        + `**Similarity:** ${response[4].similarity}\n`
-                                        + `**[${response[4].site} Page](${response[4].url})**`
-                                    )
-                                }
-                                await message.channel.send(sauceEmbed5)
+                            const sauceEmbed5 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[4].thumbnail)
+                                .setDescription(
+                                    `**Result 5**\n`
+                                    + `**By [${response[4].authorName}](${response[4].authorUrl})**\n`
+                                    + `**Site:** ${response[4].site} | `
+                                    + `**Similarity:** ${response[4].similarity}\n`
+                                    + `**[${response[4].site} Page](${response[4].url})**`
+                                )
+                            if (response[4].authorName == null) {
+                                sauceEmbed5.setDescription(
+                                    `**Result 5**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[4].site} | `
+                                    + `**Similarity:** ${response[4].similarity}\n`
+                                    + `**[${response[4].site} Page](${response[4].url})**`
+                                )
                             }
 
-                            //embed 6
-                            /*if (amount = [6, 7]) {
-                                const sauceEmbed6 = new Discord.MessageEmbed()
+                            //(currently trying to fix >5 results)
+
+                            //embed 6 
+                            /*const sauceEmbed6 = new Discord.MessageEmbed()
                                     .setColor('#ADD8E6')
                                     .setThumbnail(response[5].thumbnail)
                                     .setDescription(
@@ -179,11 +164,9 @@ module.exports = {
                                         + `**[${response[5].site} Page](${response[5].url})**`
                                     )
                                 }
-                                await message.channel.send(sauceEmbed6)
                             }
     
                             //embed 7
-                            if (amount = [7]) {
                                 const sauceEmbed7 = new Discord.MessageEmbed()
                                     .setColor('#ADD8E6')
                                     .setThumbnail(response[6].thumbnail)
@@ -202,15 +185,42 @@ module.exports = {
                                         + `**Similarity:** ${response[6].similarity}\n`
                                         + `**[${response[6].site} Page](${response[6].url})**`
                                     )
-                                }
-                                await message.channel.send(sauceEmbed7)
-                            }*/
+                                }*/
 
                             const footEmbed = new Discord.MessageEmbed()
                                 .setColor('#ADD8E6')
                                 .setDescription(`***Sauce provided by [SauceNAO.com](https://saucenao.com)***`)
                                 .setFooter('Egg-Shaped Battle Maid', client.user.displayAvatarURL())
                                 .setTimestamp()
+
+                            message.channel.send(titleEmbed)
+
+                            if (amount = 1) {
+                                await message.channel.send(sauceEmbed)
+                            }
+                            if (amount = 2) {
+                                await message.channel.send(sauceEmbed) 
+                                await message.channel.send(sauceEmbed2)
+                            }
+                            if (amount = 3) {
+                                await message.channel.send(sauceEmbed)
+                                await message.channel.send(sauceEmbed2)
+                                await message.channel.send(sauceEmbed3)
+                            }
+                            if (amount = 4) {
+                                await message.channel.send(sauceEmbed)
+                                await message.channel.send(sauceEmbed2)
+                                await message.channel.send(sauceEmbed3)
+                                await message.channel.send(sauceEmbed4)
+                            }
+                            if (amount = 5) {
+                                await message.channel.send(sauceEmbed)
+                                await message.channel.send(sauceEmbed2)
+                                await message.channel.send(sauceEmbed3)
+                                await message.channel.send(sauceEmbed4)
+                                await message.channel.send(sauceEmbed5)
+                            }
+
                             await message.channel.send(footEmbed)
 
                         } else {
