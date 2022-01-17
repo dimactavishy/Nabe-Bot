a303daccb2c2f236595ffc66ceab0e471478e1fe
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2022-01-17 04:15:21 -0500

    Update sauce.js

--- commands/search/sauce.js
+++ commands/search/sauce.js
@@ -34,164 +34,164 @@ module.exports = {
 
                         //embed 1
                         if (amount = [1, 2, 3, 4, 5, 6, 7]) {
-                        const sauceEmbed = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[0].thumbnail)
-                            .setDescription(
-                                `**Result 1**\n`
-                                + `**By [${response[0].authorName}](${response[0].authorUrl})**\n`
-                                + `**Site:** ${response[0].site} | `
-                                + `**Similarity:** ${response[0].similarity}\n`
-                                + `**[${response[0].site} Page](${response[0].url})**`
-                            )
-                        if (response[0].authorName == null) {
-                            sauceEmbed.setDescription(
-                                `**Result 1**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[0].site} | `
-                                + `**Similarity:** ${response[0].similarity}\n`
-                                + `**[${response[0].site} Page](${response[0].url})**`
-                            )
+                            const sauceEmbed = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[0].thumbnail)
+                                .setDescription(
+                                    `**Result 1**\n`
+                                    + `**By [${response[0].authorName}](${response[0].authorUrl})**\n`
+                                    + `**Site:** ${response[0].site} | `
+                                    + `**Similarity:** ${response[0].similarity}\n`
+                                    + `**[${response[0].site} Page](${response[0].url})**`
+                                )
+                            if (response[0].authorName == null) {
+                                sauceEmbed.setDescription(
+                                    `**Result 1**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[0].site} | `
+                                    + `**Similarity:** ${response[0].similarity}\n`
+                                    + `**[${response[0].site} Page](${response[0].url})**`
+                                )
+                            }
                         }
-                    }
 
                         //embed 2
                         if (amount = [2, 3, 4, 5, 6, 7]) {
-                        const sauceEmbed2 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[1].thumbnail)
-                            .setDescription(
-                                `**Result 2**\n`
-                                + `**By [${response[1].authorName}](${response[1].authorUrl})**\n`
-                                + `**Site:** ${response[1].site} | `
-                                + `**Similarity:** ${response[1].similarity}\n`
-                                + `**[${response[1].site} Page](${response[1].url})**`
-                            )
-                        if (response[1].authorName == null) {
-                            sauceEmbed2.setDescription(
-                                `**Result 2**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[1].site} | `
-                                + `**Similarity:** ${response[1].similarity}\n`
-                                + `**[${response[1].site} Page](${response[1].url})**`
-                            )
+                            const sauceEmbed2 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[1].thumbnail)
+                                .setDescription(
+                                    `**Result 2**\n`
+                                    + `**By [${response[1].authorName}](${response[1].authorUrl})**\n`
+                                    + `**Site:** ${response[1].site} | `
+                                    + `**Similarity:** ${response[1].similarity}\n`
+                                    + `**[${response[1].site} Page](${response[1].url})**`
+                                )
+                            if (response[1].authorName == null) {
+                                sauceEmbed2.setDescription(
+                                    `**Result 2**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[1].site} | `
+                                    + `**Similarity:** ${response[1].similarity}\n`
+                                    + `**[${response[1].site} Page](${response[1].url})**`
+                                )
+                            }
                         }
-                    }
 
                         //embed 3
                         if (amount = [3, 4, 5, 6, 7]) {
-                        const sauceEmbed3 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[2].thumbnail)
-                            .setDescription(
-                                `**Result 3**\n`
-                                + `**By [${response[2].authorName}](${response[2].authorUrl})**\n`
-                                + `**Site:** ${response[2].site} | `
-                                + `**Similarity:** ${response[2].similarity}\n`
-                                + `**[${response[2].site} Page](${response[2].url})**`
-                            )
-                        if (response[2].authorName == null) {
-                            sauceEmbed3.setDescription(
-                                `**Result 3**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[2].site} | `
-                                + `**Similarity:** ${response[2].similarity}\n`
-                                + `**[${response[2].site} Page](${response[2].url})**`
-                            )
+                            const sauceEmbed3 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[2].thumbnail)
+                                .setDescription(
+                                    `**Result 3**\n`
+                                    + `**By [${response[2].authorName}](${response[2].authorUrl})**\n`
+                                    + `**Site:** ${response[2].site} | `
+                                    + `**Similarity:** ${response[2].similarity}\n`
+                                    + `**[${response[2].site} Page](${response[2].url})**`
+                                )
+                            if (response[2].authorName == null) {
+                                sauceEmbed3.setDescription(
+                                    `**Result 3**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[2].site} | `
+                                    + `**Similarity:** ${response[2].similarity}\n`
+                                    + `**[${response[2].site} Page](${response[2].url})**`
+                                )
+                            }
                         }
-                    }
-                        
+
                         //embed 4
                         if (amount = [4, 5, 6, 7]) {
-                        const sauceEmbed4 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[3].thumbnail)
-                            .setDescription(
-                                `**Result 4**\n`
-                                + `**By [${response[3].authorName}](${response[3].authorUrl})**\n`
-                                + `**Site:** ${response[3].site} | `
-                                + `**Similarity:** ${response[3].similarity}\n`
-                                + `**[${response[3].site} Page](${response[3].url})**`
-                            )
-                        if (response[3].authorName == null) {
-                            sauceEmbed4.setDescription(
-                                `**Result 4**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[3].site} | `
-                                + `**Similarity:** ${response[3].similarity}\n`
-                                + `**[${response[3].site} Page](${response[3].url})**`
-                            )
+                            const sauceEmbed4 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[3].thumbnail)
+                                .setDescription(
+                                    `**Result 4**\n`
+                                    + `**By [${response[3].authorName}](${response[3].authorUrl})**\n`
+                                    + `**Site:** ${response[3].site} | `
+                                    + `**Similarity:** ${response[3].similarity}\n`
+                                    + `**[${response[3].site} Page](${response[3].url})**`
+                                )
+                            if (response[3].authorName == null) {
+                                sauceEmbed4.setDescription(
+                                    `**Result 4**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[3].site} | `
+                                    + `**Similarity:** ${response[3].similarity}\n`
+                                    + `**[${response[3].site} Page](${response[3].url})**`
+                                )
+                            }
                         }
-                    }
 
                         //embed 5
                         if (amount = [5, 6, 7]) {
-                        const sauceEmbed5 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[4].thumbnail)
-                            .setDescription(
-                                `**Result 5**\n`
-                                + `**By [${response[4].authorName}](${response[4].authorUrl})**\n`
-                                + `**Site:** ${response[4].site} | `
-                                + `**Similarity:** ${response[4].similarity}\n`
-                                + `**[${response[4].site} Page](${response[4].url})**`
-                            )
-                        if (response[4].authorName == null) {
-                            sauceEmbed4.setDescription(
-                                `**Result 5**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[4].site} | `
-                                + `**Similarity:** ${response[4].similarity}\n`
-                                + `**[${response[4].site} Page](${response[4].url})**`
-                            )
+                            const sauceEmbed5 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[4].thumbnail)
+                                .setDescription(
+                                    `**Result 5**\n`
+                                    + `**By [${response[4].authorName}](${response[4].authorUrl})**\n`
+                                    + `**Site:** ${response[4].site} | `
+                                    + `**Similarity:** ${response[4].similarity}\n`
+                                    + `**[${response[4].site} Page](${response[4].url})**`
+                                )
+                            if (response[4].authorName == null) {
+                                sauceEmbed5.setDescription(
+                                    `**Result 5**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[4].site} | `
+                                    + `**Similarity:** ${response[4].similarity}\n`
+                                    + `**[${response[4].site} Page](${response[4].url})**`
+                                )
+                            }
                         }
-                    }
 
                         //embed 6
                         if (amount = [6, 7]) {
-                        const sauceEmbed6 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[5].thumbnail)
-                            .setDescription(
-                                `**Result 6**\n`
-                                + `**By [${response[5].authorName}](${response[5].authorUrl})**\n`
-                                + `**Site:** ${response[5].site} | `
-                                + `**Similarity:** ${response[5].similarity}\n`
-                                + `**[${response[5].site} Page](${response[5].url})**`
-                            )
-                        if (response[5].authorName == null) {
-                            sauceEmbed4.setDescription(
-                                `**Result 6**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[5].site} | `
-                                + `**Similarity:** ${response[5].similarity}\n`
-                                + `**[${response[5].site} Page](${response[5].url})**`
-                            )
+                            const sauceEmbed6 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[5].thumbnail)
+                                .setDescription(
+                                    `**Result 6**\n`
+                                    + `**By [${response[5].authorName}](${response[5].authorUrl})**\n`
+                                    + `**Site:** ${response[5].site} | `
+                                    + `**Similarity:** ${response[5].similarity}\n`
+                                    + `**[${response[5].site} Page](${response[5].url})**`
+                                )
+                            if (response[5].authorName == null) {
+                                sauceEmbed6.setDescription(
+                                    `**Result 6**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[5].site} | `
+                                    + `**Similarity:** ${response[5].similarity}\n`
+                                    + `**[${response[5].site} Page](${response[5].url})**`
+                                )
+                            }
                         }
-                    }
 
                         //embed 7
                         if (amount = [7]) {
-                        const sauceEmbed7 = new Discord.MessageEmbed()
-                            .setColor('#ADD8E6')
-                            .setThumbnail(response[6].thumbnail)
-                            .setDescription(
-                                `**Result 7**\n`
-                                + `**By [${response[6].authorName}](${response[6].authorUrl})**\n`
-                                + `**Site:** ${response[6].site} | `
-                                + `**Similarity:** ${response[6].similarity}\n`
-                                + `**[${response[6].site} Page](${response[6].url})**`
-                            )
-                        if (response[6].authorName == null) {
-                            sauceEmbed4.setDescription(
-                                `**Result 7**\n`
-                                + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
-                                + `**Site:** ${response[6].site} | `
-                                + `**Similarity:** ${response[6].similarity}\n`
-                                + `**[${response[6].site} Page](${response[6].url})**`
-                            )
+                            const sauceEmbed7 = new Discord.MessageEmbed()
+                                .setColor('#ADD8E6')
+                                .setThumbnail(response[6].thumbnail)
+                                .setDescription(
+                                    `**Result 7**\n`
+                                    + `**By [${response[6].authorName}](${response[6].authorUrl})**\n`
+                                    + `**Site:** ${response[6].site} | `
+                                    + `**Similarity:** ${response[6].similarity}\n`
+                                    + `**[${response[6].site} Page](${response[6].url})**`
+                                )
+                            if (response[6].authorName == null) {
+                                sauceEmbed7.setDescription(
+                                    `**Result 7**\n`
+                                    + `**From [Imageboard](https://en.wikipedia.org/wiki/Imageboard)**\n`
+                                    + `**Site:** ${response[6].site} | `
+                                    + `**Similarity:** ${response[6].similarity}\n`
+                                    + `**[${response[6].site} Page](${response[6].url})**`
+                                )
+                            }
                         }
-                    }
 
                         const footEmbed = new Discord.MessageEmbed()
                             .setDescription(`***Sauce provided by [SauceNAO.com](https://saucenao.com)***`)
