b23837dd755abfed3ed9577597505d55db060058
Joko Sukino <79780581+dimactavishy@users.noreply.github.com>
2022-01-17 07:11:42 -0500

    Update sauce.js

--- commands/search/sauce.js
+++ commands/search/sauce.js
@@ -18,10 +18,10 @@ module.exports = {
             message.attachments.forEach(attachment => {
                 let imageurl = attachment.url;
 
-                let amount = args.join();
+                let amount = args.join(" ");
 
-                if (!amount) amount = 10;
-                if (amount > 7) return message.reply("I'm sorry, but the maximum number of sauce results is 7 to avoid channel flooding.").then(msg => { msg.delete({ timeout: 5000 }) });
+                if (!amount) amount = 5;
+                if (amount > 5) return message.reply("I'm sorry, but the maximum number of sauce results is 5 to avoid channel flooding.").then(msg => { msg.delete({ timeout: 5000 }) });
                 if (amount < 0) return message.reply("Forgive me, but specifying a number smaller than one would induce a paradox. So please, specify a greater number.").then(msg => { msg.delete({ timeout: 5000 }) });
                 if (isNaN(amount)) return message.reply("Please only enter a number between 1 to 7 or none at all.").then(msg => { msg.delete({ timeout: 5000 }) });
 
